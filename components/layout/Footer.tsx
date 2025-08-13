f8aee19056ec9ffda8b69216abfd64b107146218
arisas <arisashiraishi@outlook.com>
2025-08-13 10:56:35 -0400

    idk

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,9 @@
 export const Footer = () => (
   <footer className="bottom-0 right-0 left-0 sticky px-6 md:px-20 py-2 items-center md:py-6 max-w-8xl mx-auto flex place-items-stretch w-full self-stretch justify-stretch bg-white/10 backdrop-blur-md border-t border-white/20 shadow-lg">
-    <a href={`#`} className="text-primary font-medium font-accent text-lg flex">
+    <a
+      href={`#`}
+      className="text-primary font-medium font-accent text-sm md:text-lg flex"
+    >
       Nekonote LLC
     </a>
     <span className="ml-auto text-xs md:text-sm">
